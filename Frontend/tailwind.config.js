31e6f21bf08628e7e853ff6eedb8b0521a50b549
Aditya <s.adityaraj2204@gmail.com>
2025-09-16 17:01:09 +0530

    Restucturing

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // This tells Tailwind to scan all your JS/JSX files
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
